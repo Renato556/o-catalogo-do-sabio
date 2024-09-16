b21b2b1bbf33b310e69513a6ece31ce3faff740a
Renato <renatocabral2000@gmail.com>
2024-09-16 20:59:33 -0300

    initial commit

---
+++ src/main/java/com/f1rst/ocatalogodosabio/OCatalogoDoSabioApplication.java
@@ -0,0 +1,13 @@
+package com.f1rst.ocatalogodosabio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OCatalogoDoSabioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OCatalogoDoSabioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/f1rst/ocatalogodosabio/OCatalogoDoSabioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1rst.ocatalogodosabio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OCatalogoDoSabioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
